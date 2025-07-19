01a8411da63f307feb37eb6eaa11a4fd1f59481e
Minnu Antony <minnu.antony05@gmail.com>
2025-07-19 10:06:22 +0530

    Updated files and folder structure

---
+++ july17/hellothere.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+
+FIRST_NAME=Minnu
+LAST_NAME=Antony
+echo Hello $FIRST_NAME $LAST_NAME
---
+++ july17/ifelifelse.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+if [ ${1,,} = minnu ]; then
+	echo ohhh welcome boss!!
+elif [ ${1,,} = help ]; then
+	echo just enter your user name
+else
+	echo wrong username
+fi
+
